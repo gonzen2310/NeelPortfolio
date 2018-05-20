066d2422945f5fa5d21671ec193c4d13a79be97e
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-05-20 04:13:38 -0400

    Make migrations

--- NeelPortfolio/views.py
+++ NeelPortfolio/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("<h1>Portfolio</h1>")
+    return render(request, 'homepage.html')
